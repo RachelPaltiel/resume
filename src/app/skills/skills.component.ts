930a12115ae83c8d51cf08c8829a3c80d411ca8b
Rachel <rachel.paltiel21@gmail.com>
2022-11-08 19:59:02 +0200

    123

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  showClient() {
+    document.getElementById("client")!.style.display = "block";
+    document.getElementById("server")!.style.display = "none";
+    document.getElementById("frameWork")!.style.display = "none";
+    document.getElementById("tools")!.style.display = "none";
+  }
+
+  showServer() {
+    document.getElementById("client")!.style.display = "none";
+    document.getElementById("server")!.style.display = "block";
+    document.getElementById("frameWork")!.style.display = "none";
+    document.getElementById("tools")!.style.display = "none";
+  }
+
+  showFrameWork() {
+    document.getElementById("client")!.style.display = "none";
+    document.getElementById("server")!.style.display = "none";
+    document.getElementById("frameWork")!.style.display = "block";
+    document.getElementById("tools")!.style.display = "none";
+  }
+
+  
+  showTools() {
+    document.getElementById("client")!.style.display = "none";
+    document.getElementById("server")!.style.display = "none";
+    document.getElementById("frameWork")!.style.display = "none";
+    document.getElementById("tools")!.style.display = "block";
+  }
   constructor() { }
 
   ngOnInit(): void {
