756e6c4b97d85bdf131a3dc42d3fc3617d076256
Rachel <rachel.paltiel21@gmail.com>
2022-11-14 09:57:54 +0200

    project update

---
+++ ResumeMobile/src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { AbsoluteSourceSpan } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Routes, RoutesRecognized } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+
+export class HeaderComponent implements OnInit {
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
